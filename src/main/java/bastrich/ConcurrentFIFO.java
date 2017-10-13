370b7ff1de7a42c575d79644af7fae8736f09227
Daniil Bastrich <bastrich@bk.ru>
2017-10-13 03:01:40 +0300

    Finish

--- src/main/java/bastrich/ConcurrentFIFO.java
+++ src/main/java/bastrich/ConcurrentFIFO.java
@@ -8,15 +8,15 @@ public class ConcurrentFIFO<T> {
     private static int DEFAULT_CAPACITY = 10;
     private static int CAPACITY_STEP = 10;
 
-    private T[] storage;
-    private int last;
+    private volatile T[] storage;
+    private volatile int last;
 
     public ConcurrentFIFO() {
         storage = (T[])new Object[DEFAULT_CAPACITY];
         last = -1;
     }
 
-    public boolean add(T item) {
+    public synchronized boolean add(T item) {
         if (last >= storage.length-1) {
             T[] newStorage = (T[])new Object[storage.length + CAPACITY_STEP];
             System.arraycopy(storage, 0, newStorage, 0, storage.length);
@@ -30,7 +30,7 @@ public class ConcurrentFIFO<T> {
         return true;
     }
 
-    public T poll() {
+    public synchronized T poll() {
         if (last == -1) {
             return null;
         }
--- src/test/java/bastrich/ConcurrentFIFOTest.java
+++ src/test/java/bastrich/ConcurrentFIFOTest.java
@@ -1,10 +1,14 @@
 package bastrich;
 
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /**
  * @author bastrich on 13.10.2017.
@@ -12,11 +16,25 @@ import static org.testng.Assert.assertEquals;
 public class ConcurrentFIFOTest {
 
     private ConcurrentFIFO<Integer> concurrentFIFO;
+    private AtomicIntegerArray items = new AtomicIntegerArray(100);
+    private volatile int[][] itemsByThreads;
 
+    private final Object monitor1 = new Object();
+    private final Object monitor2 = new Object();
 
     @BeforeTest
     public void init() {
-        concurrentFIFO = new ConcurrentFIFO<Integer>();
+        concurrentFIFO = new ConcurrentFIFO<>();
+        for (int i = 1; i <= 100; i++) {
+            items.set(i-1, i);
+        }
+
+        itemsByThreads = new int[10][10];
+        for (int i = 0; i < itemsByThreads.length; i++) {
+            for (int j = 0; j < itemsByThreads[i].length; j++) {
+                itemsByThreads[i][j] = i * 10 + j + 1;
+            }
+        }
     }
 
     @Test
@@ -32,16 +50,47 @@ public class ConcurrentFIFOTest {
     }
 
 
-    @Test(threadPoolSize = 10, invocationCount = 50, timeOut = 10000)
+    @Test(threadPoolSize = 10, invocationCount = 10, timeOut = 10000)
     public void multiThreadTest() throws Exception {
-        for (int i = 1; i <= 105; i++) {
-            concurrentFIFO.add(i);
+
+        synchronized (monitor1) {
+            for (int i = 0; i < itemsByThreads.length; i++) {
+                if (itemsByThreads[i] != null) {
+                    for (int j = 0; j < itemsByThreads[i].length; j++) {
+                        concurrentFIFO.add(itemsByThreads[i][j]);
+                    }
+                    itemsByThreads[i] = null;
+                    itemsByThreads = itemsByThreads;
+                    break;
+                }
+            }
         }
 
-        for (int i = 1; i <= 105; i++) {
-            assertEquals(new Integer(i), concurrentFIFO.poll());
+        for (int i = 0; i < 10; i++) {
+            int item = concurrentFIFO.poll();
+
+            synchronized (monitor2) {
+                boolean contains = false;
+                int index = -1;
+                for (int j = 0; j < 100; j++) {
+                    if (items.get(j) == item) {
+                        contains = true;
+                        index = j;
+                        break;
+                    }
+                }
+                assertTrue(contains);
+
+                items.set(index, -1);
+            }
         }
-        assertEquals(null, concurrentFIFO.poll());
     }
 
+    @AfterClass
+    public void after() {
+        assertEquals(null, concurrentFIFO.poll());
+        for (int i = 0; i < 100; i++) {
+            assertEquals(-1, items.get(i));
+        }
+    }
 }
